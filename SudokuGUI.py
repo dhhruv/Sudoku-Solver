1eef533625f1f079b48c45e8c0d36298caed3a6f
dhhruv <dhruvnareshkumar.panchal@mail.concordia.ca>
2023-03-23 02:28:02 -0400

    Minor Bug Fixes

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,176 +1,253 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from sudokutools import valid, solve, find_empty
+from sudokutools import valid, solve, find_empty, generate_board
 from copy import deepcopy
 from sys import exit
 import pygame
 import time
 import random
+
 pygame.init()
 
 
-def generate():
-
-    while True:  
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-        board = [[0 for i in range(9)] for j in range(9)]
-
-        for i in range(9):
-            for j in range(9):
-                if random.randint(1, 10) >= 5:
-                    board[i][j] = random.randint(1, 9) 
-                    if valid(board, (i, j), board[i][j]):
-                        continue
-                    else:
-                        board[i][j] = 0
-        partialBoard = deepcopy(board)  
-        if solve(board):
-            return partialBoard
-
-
 class Board:
-
     def __init__(self, window):
-        self.board = generate()
+        """
+        Initializes a Board object.
+
+        Args:
+            window: The Pygame window object.
+        """
+        # Generate a new Sudoku board and create a solved version of it.
+        self.board = generate_board()
         self.solvedBoard = deepcopy(self.board)
         solve(self.solvedBoard)
-        self.tiles = [[Tile(self.board[i][j], window, i * 60, j * 60)
-                      for j in range(9)] for i in range(9)]
+        # Create a 2D list of Tile objects to represent the Sudoku board.
+        self.tiles = [
+            [Tile(self.board[i][j], window, i * 60, j * 60) for j in range(9)]
+            for i in range(9)
+        ]
         self.window = window
 
     def draw_board(self):
-
+        """
+        Draws the Sudoku board on the Pygame window.
+        """
         for i in range(9):
             for j in range(9):
-                if j % 3 == 0 and j != 0:  
-                    pygame.draw.line(self.window, (0, 0, 0), (j // 3
-                            * 180, 0), (j // 3 * 180, 540), 4)
-
-                if i % 3 == 0 and i != 0: 
-                    pygame.draw.line(self.window, (0, 0, 0), (0, i // 3
-                            * 180), (540, i // 3 * 180), 4)
-
+                # Draw vertical lines every three columns.
+                if j % 3 == 0 and j != 0:
+                    pygame.draw.line(
+                        self.window,
+                        (0, 0, 0),
+                        (j // 3 * 180, 0),
+                        (j // 3 * 180, 540),
+                        4,
+                    )
+                # Draw horizontal lines every three rows.
+                if i % 3 == 0 and i != 0:
+                    pygame.draw.line(
+                        self.window,
+                        (0, 0, 0),
+                        (0, i // 3 * 180),
+                        (540, i // 3 * 180),
+                        4,
+                    )
+                # Draw the Tile object on the board.
                 self.tiles[i][j].draw((0, 0, 0), 1)
 
-                if self.tiles[i][j].value != 0:  
-                    self.tiles[i][j].display(self.tiles[i][j].value,
-                            (21 + j * 60, 16 + i * 60), (0, 0, 0))  
-
-
-        pygame.draw.line(self.window, (0, 0, 0), (0, (i + 1) // 3
-                         * 180), (540, (i + 1) // 3 * 180), 4)
+                # Display the Tile value if it is not 0 (empty).
+                if self.tiles[i][j].value != 0:
+                    self.tiles[i][j].display(
+                        self.tiles[i][j].value, (21 + j * 60, 16 + i * 60), (0, 0, 0)
+                    )
+        # Draw a horizontal line at the bottom of the board.
+        pygame.draw.line(
+            self.window,
+            (0, 0, 0),
+            (0, (i + 1) // 3 * 180),
+            (540, (i + 1) // 3 * 180),
+            4,
+        )
 
     def deselect(self, tile):
+        """
+        Deselects all tiles except the given tile.
 
+        Args:
+            tile (Tile): The tile that should remain selected.
+
+        Returns:
+            None
+        """
         for i in range(9):
             for j in range(9):
                 if self.tiles[i][j] != tile:
                     self.tiles[i][j].selected = False
 
-    def redraw(
-        self,
-        keys,
-        wrong,
-        time,
-        ):
+    def redraw(self, keys, wrong, time):
+        """
+        Redraws the Sudoku board on the game window, highlighting selected, correct, and incorrect tiles, displaying the
+        current wrong count and time, and rendering the current keys (potential values) for each tile.
 
-        self.window.fill((255, 255, 255))
-        self.draw_board()
+        Args:
+            keys (dict): A dictionary containing tuples of (x, y) coordinates as keys and potential values as values.
+            wrong (int): The current wrong count.
+            time (int): The current time elapsed.
+
+        Returns:
+            None
+        """
+        self.window.fill((255, 255, 255))  # fill the window with white
+        self.draw_board()  # draw the Sudoku board
         for i in range(9):
             for j in range(9):
-                if self.tiles[j][i].selected: 
+                if self.tiles[j][i].selected:
+                    # highlight selected tiles in green
                     self.tiles[j][i].draw((50, 205, 50), 4)
                 elif self.tiles[i][j].correct:
-
+                    # highlight correct tiles in dark green
                     self.tiles[j][i].draw((34, 139, 34), 4)
                 elif self.tiles[i][j].incorrect:
-
+                    # highlight incorrect tiles in red
                     self.tiles[j][i].draw((255, 0, 0), 4)
 
-        if len(keys) != 0:  
+        if len(keys) != 0:
             for value in keys:
-                self.tiles[value[0]][value[1]].display(keys[value], (21
-                        + value[0] * 60, 16 + value[1] * 60), (128,
-                        128, 128))
+                # display the potential values for each tile
+                self.tiles[value[0]][value[1]].display(
+                    keys[value],
+                    (21 + value[0] * 60, 16 + value[1] * 60),
+                    (128, 128, 128),
+                )
 
         if wrong > 0:
-            font = pygame.font.SysFont('Bauhaus 93', 30) 
-            text = font.render('X', True, (255, 0, 0))
+            # display the current wrong count as an "X" icon and a number
+            font = pygame.font.SysFont("Bauhaus 93", 30)
+            text = font.render("X", True, (255, 0, 0))
             self.window.blit(text, (10, 554))
 
-            font = pygame.font.SysFont('Bahnschrift', 40)  
+            font = pygame.font.SysFont("Bahnschrift", 40)
             text = font.render(str(wrong), True, (0, 0, 0))
             self.window.blit(text, (32, 542))
 
-        font = pygame.font.SysFont('Bahnschrift', 40)  
+        # display the current time elapsed as a number
+        font = pygame.font.SysFont("Bahnschrift", 40)
         text = font.render(str(time), True, (0, 0, 0))
         self.window.blit(text, (388, 542))
-        pygame.display.flip()
+        pygame.display.flip()  # update the game window
 
     def visualSolve(self, wrong, time):
+        """
+        Recursively solves the Sudoku board visually, highlighting correct and incorrect tiles as it fills them in.
 
-        for event in pygame.event.get():  
+        Args:
+            wrong (int): The current wrong count.
+            time (int): The current time elapsed.
+
+        Returns:
+            bool: True if the board is successfully solved, False otherwise.
+        """
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                exit()  # exit the game if the user clicks the close button
 
         empty = find_empty(self.board)
         if not empty:
-            return True
+            return True  # the board is solved if there are no empty tiles left
 
         for nums in range(9):
             if valid(self.board, (empty[0], empty[1]), nums + 1):
+                # fill in the current empty tile with a valid number
                 self.board[empty[0]][empty[1]] = nums + 1
                 self.tiles[empty[0]][empty[1]].value = nums + 1
                 self.tiles[empty[0]][empty[1]].correct = True
-                pygame.time.delay(63)  
-                self.redraw({}, wrong, time)
+                pygame.time.delay(63)  # delay to slow down the solving animation
+                self.redraw(
+                    {}, wrong, time
+                )  # redraw the game window with the updated board
 
                 if self.visualSolve(wrong, time):
-                    return True
+                    return True  # recursively solve the rest of the board if the current move is valid
 
+                # if the current move is not valid, reset the tile and highlight it as incorrect
                 self.board[empty[0]][empty[1]] = 0
                 self.tiles[empty[0]][empty[1]].value = 0
                 self.tiles[empty[0]][empty[1]].incorrect = True
                 self.tiles[empty[0]][empty[1]].correct = False
-                pygame.time.delay(63)
-                self.redraw({}, wrong, time)
+                pygame.time.delay(63)  # delay to slow down the solving animation
+                self.redraw(
+                    {}, wrong, time
+                )  # redraw the game window with the updated board
 
     def hint(self, keys):
+        """
+        Provides a hint by filling in a random empty tile with the correct number.
 
-        while True: 
+        Args:
+            keys (dict): A dictionary containing tuples of (x, y) coordinates as keys and potential values as values.
+
+        Returns:
+            bool: True if a hint is successfully provided, False if the board is already solved.
+        """
+        while True:
             i = random.randint(0, 8)
             j = random.randint(0, 8)
-            if self.board[i][j] == 0:  
+            if self.board[i][j] == 0:
                 if (j, i) in keys:
                     del keys[(j, i)]
+                # fill in the selected empty tile with the correct number
                 self.board[i][j] = self.solvedBoard[i][j]
                 self.tiles[i][j].value = self.solvedBoard[i][j]
                 return True
             elif self.board == self.solvedBoard:
-
-                return False
+                return False  # the board is already solved, so no hint can be provided.
 
 
 class Tile:
-
-
     def __init__(
         self,
         value,
         window,
         x1,
         y1,
-        ):
+    ):
+        """
+        Initializes a Tile object.
+
+        Args:
+            value (int): The value to be displayed in the Tile.
+            window (pygame.Surface): The surface to draw the Tile on.
+            x1 (int): The x-coordinate of the top-left corner of the Tile.
+            y1 (int): The y-coordinate of the top-left corner of the Tile.
+
+        Attributes:
+            value (int): The value to be displayed in the Tile.
+            window (pygame.Surface): The surface to draw the Tile on.
+            rect (pygame.Rect): The rectangular area of the Tile.
+            selected (bool): Whether the Tile is currently selected.
+            correct (bool): Whether the value in the Tile is correct.
+            incorrect (bool): Whether the value in the Tile is incorrect.
+        """
+
         self.value = value
         self.window = window
-        self.rect = pygame.Rect(x1, y1, 60, 60)  
+        self.rect = pygame.Rect(x1, y1, 60, 60)
         self.selected = False
         self.correct = False
         self.incorrect = False
 
     def draw(self, color, thickness):
+        """
+        Draws the Tile on the window with a colored border.
+
+        Args:
+            color (tuple[int, int, int]): The RGB color value of the border.
+            thickness (int): The thickness of the border.
+
+        Returns:
+            None.
+        """
 
         pygame.draw.rect(self.window, color, self.rect, thickness)
 
@@ -179,13 +256,33 @@ class Tile:
         value,
         position,
         color,
-        ):
+    ):
+        """
+        Displays the value of the Tile in the center of the Tile.
 
-        font = pygame.font.SysFont('lato', 45)
+        Args:
+            value (int): The value to be displayed.
+            position (tuple[int, int]): The (x, y) coordinates of the center of the Tile.
+            color (tuple[int, int, int]): The RGB color value of the text.
+
+        Returns:
+            None.
+        """
+
+        font = pygame.font.SysFont("lato", 45)
         text = font.render(str(value), True, color)
         self.window.blit(text, position)
 
     def clicked(self, mousePos):
+        """
+        Checks if the Tile is clicked by the mouse.
+
+        Args:
+            mousePos (tuple[int, int]): The (x, y) coordinates of the mouse.
+
+        Returns:
+            bool: True if the Tile is clicked, False otherwise.
+        """
 
         if self.rect.collidepoint(mousePos):
             self.selected = True
@@ -193,53 +290,58 @@ class Tile:
 
 
 def main():
-
+    # Set up the pygame window
     screen = pygame.display.set_mode((540, 590))
     screen.fill((255, 255, 255))
-    pygame.display.set_caption('Sudoku Solver')
-    icon = pygame.image.load('assets/thumbnail.png')
+    pygame.display.set_caption("Sudoku Solver")
+    icon = pygame.image.load("assets/thumbnail.png")
     pygame.display.set_icon(icon)
 
-    font = pygame.font.SysFont('Bahnschrift', 40)
-    text = font.render('Generating', True, (0, 0, 0))
+    # Display "Generating Random Grid" text while generating a random grid
+    font = pygame.font.SysFont("Bahnschrift", 40)
+    text = font.render("Generating", True, (0, 0, 0))
     screen.blit(text, (175, 245))
 
-    font = pygame.font.SysFont('Bahnschrift', 40)
-    text = font.render('Random Grid', True, (0, 0, 0))
+    font = pygame.font.SysFont("Bahnschrift", 40)
+    text = font.render("Random Grid", True, (0, 0, 0))
     screen.blit(text, (156, 290))
     pygame.display.flip()
 
+    # Initialize variables
     wrong = 0
     board = Board(screen)
-    selected = (-1, -1)  
+    selected = (-1, -1)
     keyDict = {}
-    running = True
+    solved = False
     startTime = time.time()
-    while running:
+
+    # Loop until the sudoku is solved
+    while not solved:
+        # Get elapsed time and format it to display in the window
         elapsed = time.time() - startTime
-        passedTime = time.strftime('%H:%M:%S', time.gmtime(elapsed))
+        passedTime = time.strftime("%H:%M:%S", time.gmtime(elapsed))
 
-        if board.board == board.solvedBoard:  
-            for i in range(9):
-                for j in range(9):
-                    board.tiles[i][j].selected = False
-                    running = False
+        # Check if the sudoku is solved
+        if board.board == board.solvedBoard:
+            solved = True
 
+        # Handle events
         for event in pygame.event.get():
+            elapsed = time.time() - startTime
+            passedTime = time.strftime("%H:%M:%S", time.gmtime(elapsed))
             if event.type == pygame.QUIT:
-                exit()  
+                exit()
             elif event.type == pygame.MOUSEBUTTONUP:
-
+                # Check if a Tile is clicked
                 mousePos = pygame.mouse.get_pos()
                 for i in range(9):
                     for j in range(9):
                         if board.tiles[i][j].clicked(mousePos):
                             selected = (i, j)
-                            board.deselect(board.tiles[i][j])  
+                            board.deselect(board.tiles[i][j])
             elif event.type == pygame.KEYDOWN:
-
-                if board.board[selected[1]][selected[0]] == 0 \
-                    and selected != (-1, -1):
+                # Handle key presses
+                if board.board[selected[1]][selected[0]] == 0 and selected != (-1, -1):
                     if event.key == pygame.K_1:
                         keyDict[selected] = 1
 
@@ -266,47 +368,55 @@ def main():
 
                     if event.key == pygame.K_9:
                         keyDict[selected] = 9
-                    elif event.key == pygame.K_BACKSPACE or event.key \
-                        == pygame.K_DELETE:
-
+                    elif (
+                        event.key == pygame.K_BACKSPACE or event.key == pygame.K_DELETE
+                    ):
                         if selected in keyDict:
-                            board.tiles[selected[1]][selected[0]].value = \
-                                0
+                            board.tiles[selected[1]][selected[0]].value = 0
                             del keyDict[selected]
                     elif event.key == pygame.K_RETURN:
-
                         if selected in keyDict:
-                            if keyDict[selected] \
-                                != board.solvedBoard[selected[1]][selected[0]]:  
+                            if (
+                                keyDict[selected]
+                                != board.solvedBoard[selected[1]][selected[0]]
+                            ):
                                 wrong += 1
-                                board.tiles[selected[1]][selected[0]].value = \
-                                    0
+                                board.tiles[selected[1]][selected[0]].value = 0
                                 del keyDict[selected]
-                                break
+                                # break
 
-                            board.tiles[selected[1]][selected[0]].value = \
-                                keyDict[selected]  
-                            board.board[selected[1]][selected[0]] = \
-                                keyDict[selected] 
+                            board.tiles[selected[1]][selected[0]].value = keyDict[
+                                selected
+                            ]
+                            board.board[selected[1]][selected[0]] = keyDict[selected]
                             del keyDict[selected]
 
+                # Handle hint key
                 if event.key == pygame.K_h:
                     board.hint(keyDict)
 
+                # Handle space key
                 if event.key == pygame.K_SPACE:
+                    # Deselect all tiles and clear keyDict
                     for i in range(9):
                         for j in range(9):
                             board.tiles[i][j].selected = False
-                    keyDict = {}  
+                    keyDict = {}
+
+                    # Solve the sudoku visually and reset all tile correctness
+                    elapsed = time.time() - startTime
+                    passedTime = time.strftime("%H:%M:%S", time.gmtime(elapsed))
                     board.visualSolve(wrong, passedTime)
                     for i in range(9):
                         for j in range(9):
                             board.tiles[i][j].correct = False
-                            board.tiles[i][j].incorrect = False 
-                    running = False
+                            board.tiles[i][j].incorrect = False
+
+                    # Set solved to True after solving the sudoku:
+                    solved = True
 
         board.redraw(keyDict, wrong, passedTime)
-    while True:  
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
--- sudokutools.py
+++ sudokutools.py
@@ -1,65 +1,101 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from random import randint, shuffle
+
 
 def print_board(board):
-    '''Prints the board'''
+    """
+    Prints the sudoku board.
 
-    boardString = ''
+    Args:
+        board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
+
+    Returns:
+        None.
+    """
+
+    boardString = ""
     for i in range(9):
         for j in range(9):
-            boardString += str(board[i][j]) + ' '
+            boardString += str(board[i][j]) + " "
             if (j + 1) % 3 == 0 and j != 0 and j + 1 != 9:
-                boardString += '| '
+                boardString += "| "
 
             if j == 8:
-                boardString += '\n'
+                boardString += "\n"
 
             if j == 8 and (i + 1) % 3 == 0 and i + 1 != 9:
-                boardString += '- - - - - - - - - - - \n'
+                boardString += "- - - - - - - - - - - \n"
     print(boardString)
 
 
 def find_empty(board):
-    '''Finds an empty cell and returns its position as a tuple'''
+    """
+    Finds an empty cell in the sudoku board.
+
+    Args:
+        board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
+
+    Returns:
+        tuple[int, int]|None: The position of the first empty cell found as a tuple of row and column indices, or None if no empty cell is found.
+    """
 
     for i in range(9):
         for j in range(9):
             if board[i][j] == 0:
                 return (i, j)
+    return None
 
 
 def valid(board, pos, num):
-    '''Whether a number is valid in that cell, returns a bool'''
+    """
+    Checks whether a number is valid in a cell of the sudoku board.
+
+    Args:
+        board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
+        pos (tuple[int, int]): The position of the cell to check as a tuple of row and column indices.
+        num (int): The number to check.
+
+    Returns:
+        bool: True if the number is valid in the cell, False otherwise.
+    """
 
     for i in range(9):
-        if board[i][pos[1]] == num and (i, pos[1]) != pos:  # make sure it isn't the same number we're checking for by comparing coords
+        if board[i][pos[1]] == num:
             return False
 
     for j in range(9):
-        if board[pos[0]][j] == num and (pos[0], j) != pos:  # Same row but not same number
+        if board[pos[0]][j] == num:
             return False
 
-    start_i = pos[0] - pos[0] % 3  # ex. 5-5%3 = 3 and thats where the grid starts
+    start_i = pos[0] - pos[0] % 3
     start_j = pos[1] - pos[1] % 3
     for i in range(3):
-        for j in range(3):  # adds i and j as needed to go from start of grid to where we need to be
-            if board[start_i + i][start_j + j] == num and (start_i + i,
-                    start_j + j) != pos:
+        for j in range(3):
+            if board[start_i + i][start_j + j] == num:
                 return False
     return True
 
 
 def solve(board):
-    '''Solves the Sudoku board via the backtracking algorithm'''
+    """
+    Solves the sudoku board using the backtracking algorithm.
+
+    Args:
+        board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
+
+    Returns:
+        bool: True if the sudoku board is solvable, False otherwise.
+    """
 
     empty = find_empty(board)
-    if not empty:  # no empty spots are left so the board is solved
+    if not empty:
         return True
 
-    for nums in range(9):
-        if valid(board, empty, nums + 1):
-            board[empty[0]][empty[1]] = nums + 1
+    for nums in range(1, 10):
+        if valid(board, empty, nums):
+            board[empty[0]][empty[1]] = nums
 
             if solve(board):  # recursive step
                 return True
@@ -67,17 +103,68 @@ def solve(board):
     return False
 
 
-if __name__ == '__main__':
-    board =  [
-        [0, 0, 0, 0, 0, 0, 2, 0, 0],
-        [0, 8, 0, 0, 0, 7, 0, 9, 0],
-        [6, 0, 2, 0, 0, 0, 5, 0, 0],
-        [0, 7, 0, 0, 6, 0, 0, 0, 0],
-        [0, 0, 0, 9, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 2, 0, 0, 4, 0],
-        [0, 0, 5, 0, 0, 0, 6, 0, 3],
-        [0, 9, 0, 4, 0, 0, 0, 7, 0],
-        [0, 0, 6, 0, 0, 0, 0, 0, 0]
-    ]
+def generate_board():
+    """
+    Generates a random sudoku board with fewer initial numbers.
+
+    Returns:
+        list[list[int]]: A 9x9 sudoku board represented as a list of lists of integers.
+    """
+
+    board = [[0 for i in range(9)] for j in range(9)]
+
+    # Fill the diagonal boxes
+    for i in range(0, 9, 3):
+        nums = list(range(1, 10))
+        shuffle(nums)
+        for row in range(3):
+            for col in range(3):
+                board[i + row][i + col] = nums.pop()
+
+    # Fill the remaining cells with backtracking
+    def fill_cells(board, row, col):
+        """
+        Fills the remaining cells of the sudoku board with backtracking.
+
+        Args:
+            board (list[list[int]]): A 9x9 sudoku board represented as a list of lists of integers.
+            row (int): The current row index to fill.
+            col (int): The current column index to fill.
+
+        Returns:
+            bool: True if the remaining cells are successfully filled, False otherwise.
+        """
+
+        if row == 9:
+            return True
+        if col == 9:
+            return fill_cells(board, row + 1, 0)
+
+        if board[row][col] != 0:
+            return fill_cells(board, row, col + 1)
+
+        for num in range(1, 10):
+            if valid(board, (row, col), num):
+                board[row][col] = num
+
+                if fill_cells(board, row, col + 1):
+                    return True
+
+        board[row][col] = 0
+        return False
+
+    fill_cells(board, 0, 0)
+
+    # Remove a greater number of cells to create a puzzle with fewer initial numbers
+    for _ in range(randint(55, 65)):
+        row, col = randint(0, 8), randint(0, 8)
+        board[row][col] = 0
+
+    return board
+
+
+if __name__ == "__main__":
+    board = generate_board()
+    print_board(board)
     solve(board)
     print_board(board)
